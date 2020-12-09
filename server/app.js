b95b068902613d6c689290af7ea6376aa749888f
Josh Lintag <jlintag@gmail.com>
2020-12-09 11:40:43 -0500

    init working es6 express server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import { fileURLToPath } from 'url';
+const __dirname = fileURLToPath(import.meta.url);
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
